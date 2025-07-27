70bc0b7e593ee7efd09247fa1bf1d16a7cbee67c
tsnexplorer <39991490+tsnexplorer@users.noreply.github.com>
2025-07-27 19:48:27 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProjectDashboard from './ProjectDashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProjectDashboard />
+  </React.StrictMode>
+);
