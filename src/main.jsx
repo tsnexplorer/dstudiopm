05105010cb75255e56766e1f0f9e50a6e3d5c1da
TSN Explorer <tsnexplorer@gmail.com>
2025-08-01 18:05:04 +0530

    Fixed react issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import ProjectDashboard from './ProjectDashboard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div style={{ padding: "2rem" }}>
-      <h1>Debug Render Check</h1>
-    </div>
+    <ProjectDashboard />
   </React.StrictMode>
 );
